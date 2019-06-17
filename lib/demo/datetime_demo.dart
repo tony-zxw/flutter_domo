e562b11242ae32e8c18ab6aa63c559982ad90e05
tony_zhang <zhangxiaowei@moneycome.cc>
2019-06-17 11:42:54 +0800

    add intl dependencies and using DateTime

---
+++ lib/demo/datetime_demo.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; //国际化功能
+
+class DateTimeDemo extends StatefulWidget {
+  @override
+  _DateTimeDemoState createState() => _DateTimeDemoState();
+}
+
+class _DateTimeDemoState extends State<DateTimeDemo> {
+  final DateTime selectedDate = DateTime.now();
+  void _selectDate() {
+    showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2100),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DateTimeDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                InkWell(
+                  onTap: _selectDate,
+                  child: Row(
+                    children: <Widget>[
+                      Text(DateFormat.yMd().format(selectedDate)),
+                      Text(DateFormat.yMMMd().format(selectedDate)),
+                      Text(DateFormat.yMMMMd().format(selectedDate)),
+                      Icon(Icons.arrow_drop_down),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
