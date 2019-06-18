a26d61e1e2564739c1a8a3708b262cc0b26e1ee6
tony_zhang <zhangxiaowei@moneycome.cc>
2019-06-18 12:03:19 +0800

    多监听 StreamBuilder自动创建控件

--- lib/demo/stream/stream_demo.dart
+++ lib/demo/stream/stream_demo.dart
@@ -25,6 +25,9 @@ class _StreamDemoHomeState extends State<StreamHomeDemo> {
   StreamController<String> _streamDemo; //控制stream
 
   StreamSink _sinkDemo;
+
+  // String _data = '...';
+
   @override
   void dispose() {
     _streamDemo.close(); //释放
@@ -36,15 +39,22 @@ class _StreamDemoHomeState extends State<StreamHomeDemo> {
     super.initState();
     print('create a stream.');
     // Stream<String> _streamDemo = Stream.fromFuture(fetchData());//使用streamController
-    _streamDemo = StreamController<String>();
+    // _streamDemo = StreamController<String>();//单订阅
+    _streamDemo = StreamController.broadcast(); //多订阅
 
-    _sinkDemo=_streamDemo.sink;//水池 水槽的意思
+    _sinkDemo = _streamDemo.sink; //水池 水槽的意思
     print('start listening on a stream.');
     // _streamSubscription =
     //     _streamDemo.listen(onData, onError: onError, onDone: onDone);
     _streamSubscription =
         _streamDemo.stream.listen(onData, onError: onError, onDone: onDone);
+
+    _streamDemo.stream.listen(onDataTwo, onError: onError, onDone: onDone);
     print('initialize completed.');
+  }
+
+  void onDataTwo(String data) {
+    print('onDataTwo $data');
   }
 
   void onError(error) {
@@ -56,7 +66,10 @@ class _StreamDemoHomeState extends State<StreamHomeDemo> {
   }
 
   void onData(String data) {
-    print('$data');
+    // setState(() {
+    //   _data = data;
+    // });
+    print('onData $data');
   }
 
   Future<String> fetchData() async {
@@ -85,31 +98,47 @@ class _StreamDemoHomeState extends State<StreamHomeDemo> {
     String data = await fetchData();
     // _streamDemo.add(data);
 
-    _sinkDemo.add(data);//用sink来
+    _sinkDemo.add(data); //用sink来
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FlatButton(
-              child: Text('Add'),
-              onPressed: _addDataToStraem,
+            // Text('$_data'),
+            StreamBuilder(
+              stream: _streamDemo.stream,
+              initialData: '...',
+              builder: (context, snapshot) {
+                return Text('${snapshot.data}');
+              },
+            ),//动态创建，可用不使用onData中setState
+            SizedBox(
+              height: 16.0,
             ),
-            FlatButton(
-              child: Text('Pause'),
-              onPressed: _pauseStraem,
-            ),
-            FlatButton(
-              child: Text('Resume'),
-              onPressed: _resumeStraem,
-            ),
-            FlatButton(
-              child: Text('Cancel'),
-              onPressed: _cancelStraem,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FlatButton(
+                  child: Text('Add'),
+                  onPressed: _addDataToStraem,
+                ),
+                FlatButton(
+                  child: Text('Pause'),
+                  onPressed: _pauseStraem,
+                ),
+                FlatButton(
+                  child: Text('Resume'),
+                  onPressed: _resumeStraem,
+                ),
+                FlatButton(
+                  child: Text('Cancel'),
+                  onPressed: _cancelStraem,
+                ),
+              ],
             ),
           ],
         ),
