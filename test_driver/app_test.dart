49e6eb702a19cd557a21a511491ac4e8a75759bb
tony_zhang <zhangxiaowei@moneycome.cc>
2019-06-19 15:13:42 +0800

    解决package:test/test.dart问题

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,10 +1,17 @@
 import 'package:flutter_driver/flutter_driver.dart';
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:test_api/test_api.dart';
+import 'package:test/test.dart';
 
-// import 'package:test/test.dart';
 void main() {
   group('App', () {
     FlutterDriver driver;
+
+    final actionChip = find.byValueKey('actionChip');
+    final actionChipLabelText = find.byValueKey('actionChipLabelText');
+    // expect(actionChip, findsOneWidget);
+    // expect(actionChipText, findsOneWidget);
+
     setUpAll(() async {
       driver = await FlutterDriver.connect();
     });
@@ -13,6 +20,15 @@ void main() {
       if (driver != null) {
         driver.close();
       }
+    });
+
+    test('starts at 0', () async {
+      expect(await driver.getText(actionChipLabelText), '0');
+    });
+
+    test('increments the counter', () async {
+      await driver.tap(actionChip);
+      expect(await driver.getText(actionChipLabelText), '1');
     });
   });
 }
